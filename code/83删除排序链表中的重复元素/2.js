9eabec84910ff92b21c233c6193dfbafa59ffd98
man.wang <man.wang@symbio.com>
2021-02-26 23:25:50 +0800

    83删除链表中重复元素--自己的答案-已验证-错误

--- code/83删除排序链表中的重复元素/2.js
+++ code/83删除排序链表中的重复元素/2.js
@@ -10,21 +10,23 @@
  * @return {ListNode}
  */
 
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
 var deleteDuplicates = function (head) {
-  let list = head, p = list;
+  let list = null;
   while (head) {
     const val = head.val, next = head.next ? head.next.val : null;
     if (val === next) {
       head.next = head.next.next;
     }
-
-    p.next = head.next; // 1.让p获取新的一级
-    p = p.next;     // 2.又让p指向链表的下一级.p的指针又指向list及其内部，因此能够层层更新list
-
-    console.log(JSON.stringify(list))
+    list = head;
     head = head.next;
   }
-  return list;
+  return list
+  console.log(list);
 };
 
 const l = { val: 1, next: { val: 1, next: { val: 2, next: { val: 3, next: { val: 3 } } } } };
