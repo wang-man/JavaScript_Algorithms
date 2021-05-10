0f428a0e895dabcbdd154344344b61f72c12d87a
man.wang <man.wang@symbio.com>
2021-05-10 23:19:21 +0800

    二叉树最大与最小深度算法

--- code/tree/深度优先遍历dfs.js
+++ code/tree/深度优先遍历dfs.js
@@ -28,6 +28,7 @@ const tree = {
 // 深度优先遍历：将一个分支从父到孙全部遍历完再遍历兄弟节点。
 const dfs = root => {
   console.log(root.val);
+  // 在forEach中嵌套递归，这样总是沿一条线深入到底，然后再深入兄弟分支，这样就做到了深度优先
   root.children && root.children.forEach(child => {     // 可直接简写为root.children && root.children.forEach(dfs)
     dfs(child)
   });
